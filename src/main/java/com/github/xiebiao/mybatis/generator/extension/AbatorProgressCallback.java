bb8c137186e0bd2afff4c70abc978bf7823133e5
bjxieb <xiebiao@jd.com>
2016-10-12 12:06:02 +0800

    refact

--- src/main/java/com/github/xiebiao/mybatis/generator/extension/AbatorProgressCallback.java
+++ src/main/java/com/github/xiebiao/mybatis/generator/extension/AbatorProgressCallback.java
@@ -1,40 +1,41 @@
-//package com.github.xiebiao.mybatis.generator.extension;
-//
-//import org.mybatis.spring.
-//
-///**
-// * @author xiebiao
-// */
-//public class AbatorProgressCallback implements ProgressCallback {
-//  private final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(this.getClass());
-//
-//  @Override
-//  public void introspectionStarted(int totalTasks) {
-//    LOG.debug("introspectionStarted call");
-//  }
-//
-//  @Override
-//  public void generationStarted(int totalTasks) {
-//    LOG.debug("generationStarted call");
-//  }
-//
-//  @Override
-//  public void saveStarted(int totalTasks) {
-//    LOG.debug("saveStarted call");
-//  }
-//
-//  @Override
-//  public void startTask(String taskName) {
-//    LOG.debug("startTask call");
-//  }
-//
-//  @Override
-//  public void done() {
-//    LOG.debug("done call");
-//  }
-//
-//  @Override
-//  public void checkCancel() throws InterruptedException {
-//    LOG.debug("checkCancel call");
-//  }
-//}
+package com.github.xiebiao.mybatis.generator.extension;
+
+
+import org.mybatis.generator.api.ProgressCallback;
+
+/**
+ * @author xiebiao
+ */
+public class AbatorProgressCallback implements ProgressCallback {
+    private final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void introspectionStarted(int totalTasks) {
+        LOG.debug("introspectionStarted call");
+    }
+
+    @Override
+    public void generationStarted(int totalTasks) {
+        LOG.debug("generationStarted call");
+    }
+
+    @Override
+    public void saveStarted(int totalTasks) {
+        LOG.debug("saveStarted call");
+    }
+
+    @Override
+    public void startTask(String taskName) {
+        LOG.debug("startTask call");
+    }
+
+    @Override
+    public void done() {
+        LOG.debug("done call");
+    }
+
+    @Override
+    public void checkCancel() throws InterruptedException {
+        LOG.debug("checkCancel call");
+    }
+}
--- src/test/java/org/mybatis/generator/test/GeneratorAntTaskTest.java
+++ src/test/java/org/mybatis/generator/test/GeneratorAntTaskTest.java
@@ -1,25 +1,25 @@
-//package org.mybatis.generator.test;
-//
-//import java.io.IOException;
-//
-//import junit.framework.TestCase;
-//
-//import org.junit.Test;
-//import org.mybatis.generator.ant.GeneratorAntTask;
-//
-///**
-// * @author xiebiao
-// */
-//public class GeneratorAntTaskTest extends TestCase {
-//  @Test
-//  public void test_execute() throws IOException {
-//    GeneratorAntTask generatorAntTask = new GeneratorAntTask();
-//    // InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(
-//    // "mybatis_generator.xml");
-//    String file =
-//        GeneratorAntTaskTest.class.getClassLoader().getResource("").getPath()
-//            + "mybatis_generator.xml";
-//    generatorAntTask.setConfigfile(file);
-//    generatorAntTask.execute();
-//  }
-//}
+package org.mybatis.generator.test;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.mybatis.generator.ant.GeneratorAntTask;
+
+import java.io.IOException;
+
+/**
+ * @author xiebiao
+ */
+public class GeneratorAntTaskTest extends TestCase {
+    @Test
+    public void test_execute() throws IOException {
+        GeneratorAntTask generatorAntTask = new GeneratorAntTask();
+        // InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(
+        // "mybatis_generator.xml");
+        String file =
+                GeneratorAntTaskTest.class.getClassLoader().getResource("").getPath()
+                        + "mybatis_generator.xml";
+        generatorAntTask.setConfigfile(file);
+        generatorAntTask.execute();
+    }
+}
